36c9f1341efcf48c1b22548a4438d6a8f6d43329
Carlos Dueñas <carlos.duenas@Logistic-lab.com>
2025-04-25 10:45:39 -0500

    Se añade la dependencia de HttpClient para solicitudes http

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,13 @@ import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
+import { provideHttpClient } from '@angular/common/http';
 
 bootstrapApplication(AppComponent,{
   ...appConfig,
   providers: [
     ...appConfig.providers || [],
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient()
   ]
 }).catch((err) => console.error(err));
