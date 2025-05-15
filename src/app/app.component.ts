3849a93ad826ace0426b0500c17f8bb93b3b2afc
Carlos Dueñas <carlos.duenas@Logistic-lab.com>
2025-05-15 15:23:53 -0500

    Se añade Animacion del menu lateral

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,26 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
+import { trigger, transition, style, animate } from '@angular/animations';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet,RouterModule,CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+    animations: [
+    trigger('menuAnim', [
+      transition(':enter', [
+        style({ transform: 'translateX(-100%)' }),
+        animate('300ms ease-out', style({ transform: 'translateX(0)' }))
+      ]),
+      transition(':leave', [
+        animate('300ms ease-in', style({ transform: 'translateX(-100%)' }))
+      ])
+    ])
+  ]
 })
 export class AppComponent {
     menuAbierto = false;
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,18 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { appConfig } from './app/app.config';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { importProvidersFrom } from '@angular/core'; // <-- necesario
 
-bootstrapApplication(AppComponent,{
+bootstrapApplication(AppComponent, {
   ...appConfig,
   providers: [
     ...appConfig.providers || [],
     provideRouter(routes),
-    provideHttpClient()
+    provideHttpClient(),
+    importProvidersFrom(BrowserAnimationsModule) 
   ]
 }).catch((err) => console.error(err));
