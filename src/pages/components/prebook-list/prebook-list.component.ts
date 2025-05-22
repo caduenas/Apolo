438b963b033635185821da915577a9b743b3dc10
Carlos Dueñas <carlos.duenas@Logistic-lab.com>
2025-05-22 14:36:08 -0500

    [Change] Se ajusta el componente prebook  para el envio de Request y Response adecuados

--- src/pages/components/prebook-list/prebook-list.component.ts
+++ src/pages/components/prebook-list/prebook-list.component.ts
@@ -1,8 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { Component, computed, inject, OnInit, signal } from '@angular/core';
+import { Component, computed, inject, input, OnInit, signal } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FormGroup, FormBuilder } from '@angular/forms';
 import { tariffs } from '../../../app/services/tariffs.service'
+import { Ruta, Line } from '../../../app/models/tariff.model';
 
 @Component({
   selector: 'app-prebook-list',
@@ -12,15 +13,23 @@ import { tariffs } from '../../../app/services/tariffs.service'
   styleUrls: ['./prebook-list.component.scss'] 
 })
 export class PrebookListComponent implements OnInit {
-  // Llamado al Signal 
-  private api = inject(tariffs);
-  //Formulario de filtros
+  /**
+   * @param() signal: Recibe el array de las rutas desde el componente padre
+   * Es la funte principal de data de componente se inicializa en null desde el padre
+   */
+  rutasDataSignal = input.required<Ruta[] | null>();
+  /**
+   * FormGroup se usa para manejar el formulario de filtros de este componente
+   */
   filtrosForm!: FormGroup;
   tiposContenedor = [];
   rutasOriginal: any[] = [];
-  cargando = true;
   puertosSalida: string[] = [];
   puertosLlegada: string[] = [];
+  /**
+   * Este signal almacena el estado actual de los filtros aplicados por el usuario
+   * Cada ves que este signal cambia, se reevalua el computed 'rutasFiltradas'
+   */
   filtros = signal({
     origin: '',
     destination: '',
@@ -28,7 +37,16 @@ export class PrebookListComponent implements OnInit {
     precioMax: null,
     tipoContenedor: '',
   });
+  /**
+   *  Variable para mostrar un mensaje de carga. Se actualiza automaticamente
+   *  dentro del compude 'rutasFiltradas' basado en el estado de 'rutasDataSignal'  
+   */
+  cargando = true;
   constructor(private fb: FormBuilder) {}
+  /**
+   * Hook del ciclo de vida OnInit. Se ejcuta una vez cuando el componente se inicializa.
+   * Aqui se inicializa el formulario de filtros.
+   */
   ngOnInit(): void {
       this.filtrosForm = this.fb.group({
       precioMin: [''],
@@ -37,23 +55,62 @@ export class PrebookListComponent implements OnInit {
       origin: [''],
       destination: ['']
     });
-    const rutasSignal = this.api.getRoutesSignal();
-    this.rutasOriginal = [...rutasSignal()]
   }
+  /**
+   * Computed signal que filtra las rutas basandose en los criterios definidos en 'filtros'.
+   * Se reevaluara automaticamente cada vez que 'filtros' o 'rutasDataSignal' cambian. 
+   */
   rutasFiltradas = computed(()=> {
     const filtros = this.filtros();
-    const rutas = this.rutasOriginal;
-
+    const rutas = this.rutasDataSignal() || [];
+    // Se actualiza el estado de carga para la UI
+    // Si 'rutasDataSignal' es null, estamos esperando datos 
+    // Si es un array vacio, no hay rutas y no estamos cargando activamente
+    this.cargando = rutas.length === 0 && this.rutasDataSignal() === null;
     return rutas.filter(r => {
+      const precioRuta = this.getPrecioDeRuta(r);
+      const tipoContenedorRuta = this.getContenedorDeRuta(r);
       return (
+        // Se aplican los filtros uno a uno
         (!filtros.origin || r.origin === filtros.origin) &&
         (!filtros.destination || r.destination === filtros.destination) &&
-        (!filtros.precioMin || r.precio >= filtros.precioMin ) &&
-        (!filtros.precioMax || r.precio <= filtros.precioMax) &&
-        (!filtros.tipoContenedor || r.tipoContenedor === filtros.tipoContenedor)
-      )
-    })
-  })
+        (!filtros.precioMin || precioRuta >= filtros.precioMin ) &&
+        (!filtros.precioMax || precioRuta <= filtros.precioMax) &&
+        (!filtros.tipoContenedor || tipoContenedorRuta === filtros.tipoContenedor)
+      );
+    });
+  });
+
+  /**
+   * Calcula el precio total de la ruta sumando los valores de todas las lineas de cargo 
+   * @param ruta Objeto de la ruta que con tiene el array 'lines'
+   * @returns La suma total de los precios de las lineas o 0 si no hay lineas validas
+   */
+  private getPrecioDeRuta (ruta: Ruta): number {
+    let Totalprecio = 0
+    if(ruta.lines && Array.isArray(ruta.lines)){
+      for (const line of ruta.lines){
+        if (typeof line.value === 'number'){
+          Totalprecio += line.value
+        }
+      }
+    }
+    return Totalprecio; 
+  }
+  
+  /**
+   * Obtiene el tipo de contenedor de una ruta.
+   * @param ruta Objeto de la ruta.
+   * @returns El tipo de contenedor como string o null si no se encuentra
+   */
+  private getContenedorDeRuta (ruta: any): string | null {
+    return ruta.container || null
+  }
+
+  /**
+   * Aplica los filtros del formulario 'filtrosForm' a la señal 'filtros'.
+   * Esto desencadena una reevaluacion de 'rutasFiltradas'
+   */
   filtrarRutas(){
     const f = this.filtrosForm.value;
     this.filtros.set({
@@ -64,7 +121,12 @@ export class PrebookListComponent implements OnInit {
       tipoContenedor: f.tipoContenedor
     });
   }
+  /**
+   * Restablece el formulario de filtros y la señal 'filtros' a sus valores iniciales.
+   * Esto tambien desencadena una reevaluacion de 'rutasFiltradas'.
+   */
   limpiarFiltros(){
+    this.filtrosForm.reset();
     this.filtros.set({
       origin: '',
       destination: '',
