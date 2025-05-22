418c4f6bb2ab920c6ce71cc3f8b779c4dfc97ab7
Carlos Dueñas <carlos.duenas@Logistic-lab.com>
2025-05-22 14:29:49 -0500

    [Add] Se añade el uso de modelos en los servicios [Add] Comentarios y contratos de los servicios

--- src/app/services/puerto.service.ts
+++ src/app/services/puerto.service.ts
@@ -1,25 +1,43 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { BehaviorSubject, Observable } from "rxjs";
+import { Puerto } from "../models/index";
 
 @Injectable({
     providedIn: 'root'
 })
 export class PuertoService {
-    private puertosSubject = new BehaviorSubject<any[]>([]);
+    /**
+   * @private puertosSubject: BehaviorSubject<Puerto[]>
+   * Un BehaviorSubject es un tipo de Observable que necesita un valor inicial
+   * y que siempre emite su valor actual a los nuevos suscriptores inmediatamente.
+   * Lo usamos aquí para almacenar y gestionar el estado reactivo de la lista de puertos.
+   * Se inicializa con un array vacío de objetos 'Puerto'.
+   */
+    private puertosSubject = new BehaviorSubject<Puerto[]>([]);
     private puertos$ = this.puertosSubject.asObservable();
     private jsonURL = 'assets/puertos.json';
     constructor(private http: HttpClient) {}
 
-    getPuertos(): Observable<{ code: string; name: string; contry_code: string; country_code: string; country_name: string; region: string}[]>{
+    getPuertos(): Observable<Puerto[]>{
+    /**
+     * Comprueba si el BehaviorSubject ya tiene datos (es decir, si los puertos ya se cargaron).
+     * Si el array de valor actual está vacío, significa que es la primera vez que se solicita
+     * o que aún no se han cargado los datos.*/
         if (this.puertosSubject.value.length === 0){
             this.cargarPuertos();
         }
         return this.puertos$;
     }
 
+    /**
+   * @private cargarPuertos(): void
+   * Método privado encargado de realizar la petición HTTP para obtener los datos de los puertos
+   * desde el archivo JSON.
+   * Una vez que los datos son recibidos, los emite a través del 'puertosSubject'.
+   */
     private cargarPuertos(): void {
-        this.http.get<{ code: string; name: string; contry_code: string; country_code: string; country_name: string; region: string}[]>(this.jsonURL).subscribe({
+        this.http.get<Puerto[]>(this.jsonURL).subscribe({
             next: (data) => {
                 console.log('Puertos cargados correctamente');
                 this.puertosSubject.next(data);
--- src/app/services/tariffs.service.ts
+++ src/app/services/tariffs.service.ts
@@ -1,24 +1,24 @@
 import { Injectable, inject } from "@angular/core";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { signal } from "@angular/core";
-import { toSignal } from "@angular/core/rxjs-interop"
 import { tap } from "rxjs/operators"
+import { Ruta } from "../models";
 
 
 @Injectable({providedIn: 'root'})
 export class tariffs {
     private http = inject(HttpClient)
-    routes = signal<any[]>([]);
+    routes = signal<Ruta[]>([]);
 
-    postData(body: any){
-        const url = 'https://eos-api-llab-qa.azurewebsites.net/api/GetTariffRates';
+    postData(body: Ruta){
+        const url = '/api/GetTariffRates';
         const username = 'test1';
         const password = 'test1'
         const headers = new HttpHeaders({
             'Content-Type': 'application/json',
             'Authorization': 'Basic ' + btoa(`${username}:${password}`)
         });
-        return this.http.post<any[]>(url, body, { headers }).pipe(
+        return this.http.post<Ruta[]>(url, body, { headers }).pipe(
             tap((data) => this.routes.set(data))
         );
     }
