db7046a5566c85e4a1743099abf6eeab2f9521d5
Carlos Dueñas <carlos.duenas@Logistic-lab.com>
2025-06-09 17:04:58 -0500

    Logica de objeto signal

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from '../pages/components/home/home.component';
 import { CalculatorComponent } from '../pages/components/calculator/calculator.component';
+import { DetailsRouteComponent } from '../pages/components/details-route/details-route.component';
 
 export const routes: Routes = [
   { path: 'inicio', component: HomeComponent },
   { path: 'calculator', component: CalculatorComponent },
+  { path: 'detalle', component: DetailsRouteComponent },
   { path: '**', redirectTo: 'calculator' }
 ];
