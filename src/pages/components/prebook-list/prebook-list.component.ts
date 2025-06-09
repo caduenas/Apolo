c038dc1635cb78c82bd01d1637bde6fe6ab0d8b9
Carlos Dueñas <carlos.duenas@Logistic-lab.com>
2025-06-09 17:03:42 -0500

    Se cambia logica del uso de Signal para el manejo de las rutas cargadas desde el Endpoint

--- src/pages/components/prebook-list/prebook-list.component.ts
+++ src/pages/components/prebook-list/prebook-list.component.ts
@@ -4,6 +4,8 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FormGroup, FormBuilder } from '@angular/forms';
 import { tariffs } from '../../../app/services/tariffs.service'
 import { Ruta, Line } from '../../../app/models/tariff.model';
+import { Router } from '@angular/router';
+import { RouteStore } from '../../../app/services/routeStore';
 
 @Component({
   selector: 'app-prebook-list',
@@ -13,6 +15,10 @@ import { Ruta, Line } from '../../../app/models/tariff.model';
   styleUrls: ['./prebook-list.component.scss'] 
 })
 export class PrebookListComponent implements OnInit {
+  private router = inject(Router);
+  private routeStore = inject(RouteStore)
+
+
   /**
    * @param() signal: Recibe el array de las rutas desde el componente padre
    * Es la funte principal de data de componente se inicializa en null desde el padre
@@ -41,7 +47,9 @@ export class PrebookListComponent implements OnInit {
    *  Variable para mostrar un mensaje de carga. Se actualiza automaticamente
    *  dentro del compude 'rutasFiltradas' basado en el estado de 'rutasDataSignal'  
    */
-  cargando = true;
+  cargando = computed(()=> {
+    return this.rutasDataSignal() == null
+  })
   constructor(private fb: FormBuilder) {}
   /**
    * Hook del ciclo de vida OnInit. Se ejcuta una vez cuando el componente se inicializa.
@@ -66,7 +74,6 @@ export class PrebookListComponent implements OnInit {
     // Se actualiza el estado de carga para la UI
     // Si 'rutasDataSignal' es null, estamos esperando datos 
     // Si es un array vacio, no hay rutas y no estamos cargando activamente
-    this.cargando = rutas.length === 0 && this.rutasDataSignal() === null;
     return rutas.filter(r => {
       const precioRuta = this.getPrecioDeRuta(r);
       const tipoContenedorRuta = this.getContenedorDeRuta(r);
@@ -86,7 +93,7 @@ export class PrebookListComponent implements OnInit {
    * @param ruta Objeto de la ruta que con tiene el array 'lines'
    * @returns La suma total de los precios de las lineas o 0 si no hay lineas validas
    */
-  private getPrecioDeRuta (ruta: Ruta): number {
+  public getPrecioDeRuta (ruta: Ruta): number {
     let Totalprecio = 0
     if(ruta.lines && Array.isArray(ruta.lines)){
       for (const line of ruta.lines){
@@ -136,7 +143,9 @@ export class PrebookListComponent implements OnInit {
     });
   }
 
-  prebook(ruta: any) {
+  prebook(ruta: Ruta) {
+    this.routeStore.setSelectedRoute(ruta);
+    this.router.navigate(['/detalle']);
     console.log("Prebook", ruta);
   }
 }
